7e88e2aaed3e185d0464772b3835cc71521f9415
Luis Gordon <luisdgordon@gmail.com>
2022-10-31 03:02:37 -0500

    Consulta a api realizada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import Navbar from './components/Navbar';
+import {useEffect, useState} from 'react';
 
+function App () {
 
-function App() {
-  return (
-   <h1>JUEGOS APP</h1>
-  );
+  const [characters, setcharacters] = useState([])
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon"
+  const fetchCharacters = (Url) => {
+    fetch (Url)
+      .then (response => response.json ())
+      .then (data => setcharacters(data.results))
+      .catch (error => console.log (error));
+  };
+  useEffect (() => {}, 
+    fetchCharacters(initialUrl),
+  []);
+
+  return <Navbar brand={'Videojuegos'} />;
 }
 
 export default App;
