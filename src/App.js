88ebfefcfa7936f09d209ed9f5784b5832d1dad0
Luis Gordon <luisdgordon@gmail.com>
2022-10-31 14:20:28 -0500

    Clase Characters modificada

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import Navbar from './components/Navbar';
-import {useEffect, useState} from 'react';
+import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+import { Characters } from "./components/Characters";
 
-function App () {
-
-  const [characters, setcharacters] = useState([])
-  const initialUrl = "https://pokeapi.co/api/v2/pokemon"
+function App() {
+  const [characters, setcharacters] = useState([]);
+  const initialUrl = "https://xivapi.com/item";
   const fetchCharacters = (Url) => {
-    fetch (Url)
-      .then (response => response.json ())
-      .then (data => setcharacters(data.results))
-      .catch (error => console.log (error));
+    fetch(Url)
+      .then((response) => response.json())
+      .then((data) => setcharacters(data.Results))
+      .catch((error) => console.log(error));
   };
-  useEffect (() => {}, 
-    fetchCharacters(initialUrl),
-  []);
+  useEffect(() => {fetchCharacters(initialUrl)}, []);
 
-  return <Navbar brand={'Videojuegos'} />;
+  return (
+    <>
+      <Navbar brand={"Videojuegos"} />
+      <div className="container">
+          <Characters characters = {characters} />
+      </div>
+      
+    </>
+  );
 }
 
 export default App;
